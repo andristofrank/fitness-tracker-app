1e28b1eb3a8def32d9cc27893e0708ac6872f5a4
andristofrank <andrei.tofan8@gmail.com>
2021-04-12 16:03:57 +0200

    routing for logout
    methods in the subsequent components calling the authService

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -24,6 +24,11 @@ export class SidenavListComponent implements OnInit, OnDestroy {
     this.closeSidenav.emit();
   }
 
+  onLogout() {
+    this.onNavigationItemClicked();
+    this.authService.logout();
+  }
+
   ngOnDestroy() {
     this.authSubscription.unsubscribe();
   }
