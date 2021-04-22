4dbf6d8bde58eac1946809f91e7e0fe95e33a8a4
andristofrank <andrei.tofan8@gmail.com>
2021-04-22 19:16:59 +0200

    implement form to new training component
    to be able to choose an exercise from the dropdown and connect that with the service for starting an exercise in the new training

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../exercise.service';
 
@@ -18,8 +19,8 @@ export class NewTrainingComponent implements OnInit {
     this.exercises = this.trainingService.getAvailableExercises();
   }
 
-  onStartTraining() {
-    this.trainingService.startExercise();
+  onStartTraining(form: NgForm) {
+    this.trainingService.startExercise(form.value.exercise);
   }
 
 }
