3506ab1f248e59b3c4a1f50e9c71b2b9cbacb3cd
andristofrank <andrei.tofan8@gmail.com>
2021-04-22 19:54:37 +0200

    implemented methods for complete and cancelled exercise
    so that thy can be used appropriately in the current training component

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 import { TrainingService } from '../exercise.service';
@@ -11,7 +11,7 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
-  @Output() trainingExit = new EventEmitter();
+  
   progress = 0;
   timer: number;
 
@@ -26,6 +26,7 @@ export class CurrentTrainingComponent implements OnInit {
     this.timer = setInterval(() => {
       this.progress = this.progress + 10;
       if(this.progress >= 100) {
+        this.trainingService.completeExercise();
         clearInterval(this.timer);
       }
     }, step);
@@ -41,7 +42,7 @@ export class CurrentTrainingComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if(result) {
-        this.trainingExit.emit();
+        this.trainingService.cancelExercise(this.progress);
       } else {
         this.startOrResumeTimer();
       }
--- src/app/training/exercise.service.ts
+++ src/app/training/exercise.service.ts
@@ -12,6 +12,7 @@ export class TrainingService {
         { id: 'burpees', name: 'Burpees', duration: 60, calories: 88 },
     ];
     private activeExercise: Exercise;
+    private exercises: Exercise[] = [];
 
     getAvailableExercises(){
         return this.availableExercise.slice();
@@ -22,6 +23,28 @@ export class TrainingService {
             ex => ex.id === selectedId
         );
         this.exerciseChanged.next({ ...this.activeExercise });
+    }
+
+    completeExercise() {
+        this.exercises.push({ 
+            ...this.activeExercise, 
+            date: new Date(), 
+            state: 'completed' 
+        });
+        this.activeExercise = null;
+        this.exerciseChanged.next(null);
+    }
+
+    cancelExercise(progress: number) {
+        this.exercises.push({ 
+            ...this.activeExercise,
+            duration: this.activeExercise.duration * (progress / 100),
+            calories: this.activeExercise.calories * (progress / 100),
+            date: new Date(), 
+            state: 'cancelled' 
+        });
+        this.activeExercise = null;
+        this.exerciseChanged.next(null);
     }
 
     getActiveExercise() {
