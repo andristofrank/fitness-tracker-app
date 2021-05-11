8af8d43f08b195ec050c71bb37d8d5f1ea8bc353
andristofrank <andrei.tofan8@gmail.com>
2021-05-11 23:29:44 +0200

    another update on exercise.service
    to be able to fetchCompletedOrCancelledExercises from the database and keep track of how these exercises change in order to show them in the past-training data table

--- src/app/training/exercise.service.ts
+++ src/app/training/exercise.service.ts
@@ -9,11 +9,11 @@ import { map } from 'rxjs/operators';
 export class TrainingService {
     exerciseChanged = new Subject<Exercise>();
     exercisesChanged = new Subject<Exercise[]>();
+    finishedExercisesChanged = new Subject<Exercise[]>();
 
     private availableExercises: Exercise[] = [];
 
     private activeExercise: Exercise;
-    private exercises: Exercise[] = [];
 
     constructor(private db: AngularFirestore) {}
 
@@ -69,8 +69,13 @@ export class TrainingService {
         return {...this.activeExercise};
     }
 
-    getCompletedOrCancelledExercises() {
-        return this.exercises.slice();
+    fetchCompletedOrCancelledExercises() {
+        this.db
+            .collection('finishedExercises')
+            .valueChanges()
+            .subscribe((exercises: Exercise[]) => {
+                this.finishedExercisesChanged.next(exercises);
+        })
     }
 
     private addDataToDatabase(exercise: Exercise) {
