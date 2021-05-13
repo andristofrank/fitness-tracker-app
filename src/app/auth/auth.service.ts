0525e1648b6d6a0644d26ebd91111e7537782de5
andristofrank <andrei.tofan8@gmail.com>
2021-05-13 14:39:12 +0200

    working on services and authentication
    handle the case when user logs out but the subscriptions are still running in the background and implement a global method for when user is or not authenticated and what happens in each case

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,49 +1,66 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 import { AuthData } from "./auth-data.model";
 import { User } from "./user.model";
+import { TrainingService } from '../training/exercise.service';
 
 @Injectable()
 export class AuthService {
     authChange = new Subject<boolean>();
-    private user: User;
+    private isAuthenticated = false;
 
-    constructor(private router: Router ) {}
+    constructor(
+        private router: Router, 
+        private angularfireAuth: AngularFireAuth, 
+        private trainingService: TrainingService 
+    ) {}
+
+    initAuthListener() {
+        this.angularfireAuth.authState.subscribe(user => {
+            if(user) {
+                this.isAuthenticated = true;
+                this.authChange.next(true);
+                this.router.navigate(['/training']);
+            } else {
+                this.trainingService.cancelSubscriptions();
+                this.authChange.next(false);
+                this.router.navigate(['/login']);
+                this.isAuthenticated = false;
+            }
+        });
+    }
 
     registerUser(authData: AuthData) {
-        this.user = {
-            email: authData.email,
-            userId: Math.round(Math.random() * 1000).toString()
-        };
-        this.authSuccessfully();
+        this.angularfireAuth.createUserWithEmailAndPassword(
+            authData.email, 
+            authData.password
+        ).then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 
     login(authData: AuthData) {
-        this.user = {
-            email: authData.email,
-            userId: Math.round(Math.random() * 1000).toString()
-        };
-        this.authSuccessfully();
+        this.angularfireAuth
+            .signInWithEmailAndPassword(authData.email, authData.password)
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     logout() {
-        this.user = null;
-        this.authChange.next(false);
-        this.router.navigate(['/login']);
-    }
-
-    getUser() {
-        return { ...this.user };
+        this.angularfireAuth.signOut();
     }
 
     isAuth() {
-        return this.user != null;
-    }
-
-    private authSuccessfully() {
-        this.authChange.next(true);
-        this.router.navigate(['/training']);
+        return this.isAuthenticated;
     }
 }
