2df36ca41242463ddd8858bff617cf0df9b70bf3
andristofrank <andrei.tofan8@gmail.com>
2021-08-03 16:25:52 +0200

    prepare build for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCzvMFdn-I27cSFD6YA4uIJt-EzD1_b8NA',
+    authDomain: 'ng-fitness-tracker-c06a4.firebaseapp.com',
+    projectId: 'ng-fitness-tracker-c06a4',
+    storageBucket: 'ng-fitness-tracker-c06a4.appspot.com',
+    messagingSenderId: '688870073321',
+    appId: '1:688870073321:web:8ffddcd5e25fe7c1692178',
+    measurementId: 'G-5BQWFGKLFF'
+  }
 };
